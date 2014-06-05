646fb47b58e8ca6f6232192f09906221b4ca94e7
Alej. N. Pérez <alxegod@gmail.com>
2014-06-06 01:03:50 +0200

    refactored navbar "sections" navigation to delegate to a different file for clarity purposes.
    fixed the 'extends' from archive and changed panel color of base
    
    tried, with no result, to add a javascript date-picker to home view.
    
    modified urls to use a list instead of  urlpatterns
    
    fixed taskcreate form to exclude fields and save author as logged in user.

--- TaskrP/taskr/forms.py
+++ TaskrP/taskr/forms.py
@@ -4,7 +4,9 @@ from .models import Task
 
 class TaskCreateForm(forms.ModelForm):
     title = forms.CharField(help_text=u'')
-    deadline = forms.DateTimeField(help_text=u'Optional, format: yyyy/mm/dd (HH:MM)')
+    deadline = forms.DateTimeField(help_text=u'Optional, format: yyyy/mm/dd (HH:MM)',
+                                   widget=forms.DateTimeInput(attrs={'id': 'datepicker'}))
 
     class Meta:
         model = Task
+        exclude = ['author', 'completed']
--- TaskrP/taskr/urls.py
+++ TaskrP/taskr/urls.py
@@ -1,21 +1,24 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.core.urlresolvers import reverse_lazy
 
 from taskr.views import IndexView, HomeView, TaskListView, TaskArchiveView, TaskDetailView, TaskCompleteView
 from accounts.views import LoginView, LogoutView, RegisterView
 
-urlpatterns = patterns('',
-       url(r'^auth/login/$', LoginView.as_view(template_name='auth/login.html'), name='auth-login'),
-       url(r'^auth/logout/$', LogoutView.as_view(), name='auth-logout'),
-       url(r'^auth/register/$', RegisterView.as_view(template_name='auth/register.html'), name='auth-register'),
+urlpatterns = [
+    url(r'^auth/login/$', LoginView.as_view(template_name='auth/login.html'), name='auth-login'),
+    url(r'^auth/logout/$', LogoutView.as_view(), name='auth-logout'),
+    url(r'^auth/register/$', RegisterView.as_view(template_name='auth/register.html'), name='auth-register'),
 
-       url(r'^task/list/$', TaskListView.as_view(), name='task-list'),
-       url(r'^task/archive/$', TaskArchiveView.as_view(), name='task-archive'),
-       url(r'^task/(?P<pk>[1-9][0-9]*)/$', TaskDetailView.as_view(), name='task-detail'),
-       url(r'^task/(?P<pk>[1-9][0-9]*)/complete/$', TaskCompleteView.as_view(), name='task-complete'),
+    # url(r'^profile/$', ProfileView.as_view(), name='author-profile-self' ),
+    # url(r'^profile/(?P<pk>[1-9][0-9]*)/$', ProfileView.as_view(), name='author-profile'),
 
-       url(r'^users/(?P<pk>[1-9][0-9]*)/', IndexView.as_view(), name='user-profile'),
+    url(r'^task/list/$', TaskListView.as_view(), name='task-list'),
+    url(r'^task/archive/$', TaskArchiveView.as_view(), name='task-archive'),
+    url(r'^task/(?P<pk>[1-9][0-9]*)/$', TaskDetailView.as_view(), name='task-detail'),
+    url(r'^task/(?P<pk>[1-9][0-9]*)/complete/$', TaskCompleteView.as_view(), name='task-complete'),
 
-       url(r'^home/$', HomeView.as_view(), name='home'),
-       url(r'^$', IndexView.as_view(), name='index'),
-)
+    url(r'^users/(?P<pk>[1-9][0-9]*)/', IndexView.as_view(), name='user-profile'),
+
+    url(r'^home/$', HomeView.as_view(), name='home'),
+    url(r'^$', IndexView.as_view(), name='index'),
+]
