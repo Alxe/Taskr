f2350ffeefbc7552f0ba63a62cb9e3480f01edca
Alex <alxegod@gmail.com>
2014-06-05 17:54:15 +0200

    added a custom backend to 'accounts' auth app

---
+++ TaskrP/accounts/backends.py
@@ -0,0 +1,24 @@
+from .models import User
+
+
+class EmailAuthBackend(object):
+    """ Backend to provide authentication to our module's custom User """
+
+    def authenticate(self, email=None, password=None):
+        """ Return the user with the provided data, or None if not found """
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        """ Returns a user given it's primary key, or None if not found"""
+        try:
+            user = User.objects.get(pk=user_id)
+            if user.is_active:
+                return user
+            return None
+        except User.DoesNotExist:
+            return None
