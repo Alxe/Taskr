692f7c283986b69209b89a638093afb452a13a4a
Alej. N. Pérez <alxegod@gmail.com>
2014-06-10 23:12:16 +0200

    organized/optimized project settings. so pretty and things.

--- TaskrP/taskrp/settings.py
+++ TaskrP/taskrp/settings.py
@@ -1,26 +1,25 @@
-"""
-Django settings for TaskrP project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django.core.urlresolvers import reverse_lazy
+
+# Base Configuration
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+ROOT_URLCONF = 'taskrp.urls'
+WSGI_APPLICATION = 'taskrp.wsgi.application'
+STATIC_URL = '/static/'
 
+# Database Configuration
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Security Configuration
 SECRET_KEY = '#n#pf-*)rn3^1!lz854@w+ppr%gq7+g1&(dg*!*34s4l0=1uq2'
+DEBUG = False
+TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = ['*']
-DEBUG = True
-TEMPLATE_DEBUG = True
-
-# Auth settings
-AUTH_USER_MODEL = 'accounts.User'
-AUTHENTICATION_BACKENDS = ('accounts.backends.EmailIdentityBackend', )
 
 # Installed applications
 INSTALLED_APPS = (
@@ -31,18 +30,20 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.staticfiles',
+
+    # 3rd party apps
+    'debug_toolbar',
+    'south',
+    'rest_framework',
+    'bootstrap3',
 
     # 1st party apps
     'accounts',
     'taskr',
-
-    # 3rd party apps
-    'south',
-    'rest_framework',
-    'bootstrap3',
+    'taskr_rest',
 )
 
+# Middleware Configuration
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,49 +53,31 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'taskrp.urls'
-
-WSGI_APPLICATION = 'taskrp.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_URL = '/static/'
-
-# Login/Logout
-LOGIN_REDIRECT_URL = reverse_lazy('taskr:index')
-LOGIN_URL = reverse_lazy('taskr:auth-login')
-LOGOUT_URL = reverse_lazy('taskr:auth-logout')
-
+# Template Processor Configuration
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 TEMPLATE_CONTEXT_PROCESSORS = TCP + (
     'django.core.context_processors.request',
 )
 
+# Auth Configuration
+from django.conf.global_settings import AUTHENTICATION_BACKENDS as AUTHB
+AUTHENTICATION_BACKENDS = ('accounts.backends.EmailIdentityBackend', ) + AUTHB
+AUTH_USER_MODEL = 'accounts.User'
+LOGIN_URL = reverse_lazy('taskr:auth-login')
+LOGOUT_URL = reverse_lazy('taskr:auth-logout')
+LOGIN_REDIRECT_URL = reverse_lazy('taskr:index')
+
+# Datetime Configuration
+from django.conf.global_settings import DATETIME_INPUT_FORMATS as DIF
+DATETIME_INPUT_FORMATS = (
+    '%Y/%m/%d %H:%M',        # '2006/10/25 14:30'
+    '%Y/%m/%d',              # '2006/10/25'
+    '%y/%m/%d %H:%M',        # '06/10/25 14:30'
+    '%y/%m/%d',              # '06/10/25'
+) + DIF
+
+
+## Third party settings
 # Django-boostrap3
 BOOTSTRAP3 = {
     'theme_url': '//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css',
